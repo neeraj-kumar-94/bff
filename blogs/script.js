0bbaae79a391ab0d6dbd09641d6ab372edd3e663
neeraj-kumar-94 <neeraj74530@gmail.com>
2024-06-18 14:42:41 +0530

    blog page add cards

--- blogs/script.js
+++ blogs/script.js
@@ -16,39 +16,3 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-
-
-// accordion section
-const ourItemDiv = document.getElementsByClassName("acc-box");
-const openIcon = document.getElementsByClassName("iconOpen");
-const closeIcon = document.getElementsByClassName("iconClose");
-
-// Initially hide all close icons
-for (let i = 0; i < closeIcon.length; i++) {
-  closeIcon[i].style.display = "none";
-}
-
-for (let i = 0; i < ourItemDiv.length; i++) {
-  ourItemDiv[i].addEventListener("click", () => {
-    // First, close all items
-    for (let j = 0; j < ourItemDiv.length; j++) {
-      if (j !== i) {
-        // Don't toggle the clicked item
-        ourItemDiv[j].classList.remove("active");
-        closeIcon[j].style.display = "none";
-        openIcon[j].style.display = "block";
-      }
-    }
-
-    // Then, toggle the clicked item
-    const result = ourItemDiv[i].classList.toggle("active");
-
-    if (result) {
-      closeIcon[i].style.display = "block";
-      openIcon[i].style.display = "none";
-    } else {
-      closeIcon[i].style.display = "none";
-      openIcon[i].style.display = "block";
-    }
-  });
-}
