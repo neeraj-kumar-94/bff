abecf7d6708e0ea4c4e179e298d624f3e92853e7
neeraj-kumar-94 <neeraj74530@gmail.com>
2024-06-20 19:08:30 +0530

    in post index page add slider

--- blogs/script.js
+++ blogs/script.js
@@ -1,18 +1,32 @@
 // navbar
 // navbar
-document.addEventListener('DOMContentLoaded', function () {
-  const menuIcon = document.getElementById('humburger-icon');
-  const closeIcon = document.getElementById('close-icon');
-  const sidebar = document.querySelector('.mobile-sidebar');
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.getElementById("humburger-icon");
+  const closeIcon = document.getElementById("close-icon");
+  const sidebar = document.querySelector(".mobile-sidebar");
 
-  menuIcon.addEventListener('click', function () {
-      sidebar.style.left = '0';
-      closeIcon.style.display = 'block';
+  menuIcon.addEventListener("click", function () {
+    sidebar.style.left = "0";
+    closeIcon.style.display = "block";
   });
 
-  closeIcon.addEventListener('click', function () {
-      sidebar.style.left = '-310px';
-      closeIcon.style.display = 'none';
+  closeIcon.addEventListener("click", function () {
+    sidebar.style.left = "-310px";
+    closeIcon.style.display = "none";
   });
 });
 
+// Blogs slider
+var swiper = new Swiper(".post-slider", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+});
