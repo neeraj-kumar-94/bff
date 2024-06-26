0c6fa4140458a1dcd427cdc029fae684bf9b8aa6
neeraj-kumar-94 <neeraj74530@gmail.com>
2024-06-26 18:16:05 +0530

    change navbar button

--- login/script.js
+++ login/script.js
@@ -1,5 +1,5 @@
-// navbar
-// navbar
+// navbar section start
+// ====================
 document.addEventListener("DOMContentLoaded", function () {
   const menuIcon = document.getElementById("humburger-icon");
   const closeIcon = document.getElementById("close-icon");
@@ -16,3 +16,39 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+// Navbar active state underline
+document.addEventListener("DOMContentLoaded", function() {
+  const navLinks = document.querySelectorAll(".nav-link");
+  const currentUrl = window.location.href;
+
+  navLinks.forEach(link => {
+      if (link.href === currentUrl) {
+          link.classList.add("active");
+      }
+  });
+});
+
+// Read button dropdown
+document.addEventListener("DOMContentLoaded", function () {
+  let readMoreButton = document.querySelector(".nav-dw-btn");
+  let closeArrow = document.querySelector(".close-arrow");
+  let openArrow = document.querySelector(".open-arrow");
+  let hideLinks = document.querySelector(".nav-hidden-links");
+
+  readMoreButton.addEventListener("click", function () {
+    if (hideLinks.style.display === "block") {
+      hideLinks.style.display = "none";
+      closeArrow.style.display = "block";
+      openArrow.style.display = "none";
+      readMoreButton.classList.remove("active");
+    } else {
+      hideLinks.style.display = "block";
+      closeArrow.style.display = "none";
+      openArrow.style.display = "block";
+      readMoreButton.classList.add("active");
+    }
+  });
+});
+
+// navbar section end
+// ====================
